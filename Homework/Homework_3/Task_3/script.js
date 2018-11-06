f1c110307b7d87e8501346177dfd73cf4b777a54
Dmytro <dnarizhnyi94@gmail.com>
2018-11-06 19:25:45 +0200

    new test

--- Homework/Homework_3/Task_3/script.js
+++
@@ -1,9 +0,0 @@
-let isEven=false;
-
-while (!isEven){
-    let number = prompt('You will forever enter uneven numbers');
-    if (number%2==0){
-        isEven=true;
-        alert ("You are free!");
-    }
-}
